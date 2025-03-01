a6e1ba6323000af687e992d349a02263f35ca17e
dioKR <azss0470@gmail.com>
2025-03-01 20:20:44 +0900

    feat: expo-video를 이용한 영상 출력

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,45 +1,46 @@
+import React from "react";
 import { colors } from "@/styles/colors";
 import { Tabs } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
 
 export default function TabLayout() {
-  return (
-    <>
-      {/* 안드로이드 배너 색상 맞추기 위해 사용*/}
-      <StatusBar style="light" backgroundColor={colors.green[100]} />
-      <Tabs
-        screenOptions={{
-          headerShown: true,
-          headerStyle: {
-            backgroundColor: colors.green[100],
-          },
-          headerTintColor: "#fff",
-          headerTitleStyle: {
-            fontWeight: "bold",
-          },
-          tabBarActiveTintColor: colors.green[100],
-        }}
-      >
-        <Tabs.Screen
-          name="index"
-          options={{
-            title: "주간 운동",
-          }}
-        />
-        <Tabs.Screen
-          name="previous"
-          options={{
-            title: "지난 운동 기록",
-          }}
-        />
-        <Tabs.Screen
-          name="exercise"
-          options={{
-            title: "동작 별 기록",
-          }}
-        />
-      </Tabs>
-    </>
-  );
+	return (
+		<>
+			{/* 안드로이드 배너 색상 맞추기 위해 사용*/}
+			<StatusBar style="light" backgroundColor={colors.green[100]} />
+			<Tabs
+				screenOptions={{
+					headerShown: true,
+					headerStyle: {
+						backgroundColor: colors.green[100],
+					},
+					headerTintColor: "#fff",
+					headerTitleStyle: {
+						fontWeight: "bold",
+					},
+					tabBarActiveTintColor: colors.green[100],
+				}}
+			>
+				<Tabs.Screen
+					name="index"
+					options={{
+						title: "주간 운동",
+					}}
+				/>
+				<Tabs.Screen
+					name="previous"
+					options={{
+						title: "지난 운동 기록",
+					}}
+				/>
+				<Tabs.Screen
+					name="exercise"
+					options={{
+						title: "동작 별 기록",
+					}}
+				/>
+			</Tabs>
+		</>
+	);
 }
--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -1,33 +1,69 @@
 import { useRef } from "react";
-import { Dimensions, StyleSheet } from "react-native";
-import Video, { VideoRef } from "react-native-video";
+import { useVideoPlayer, VideoSource, VideoView } from "expo-video";
+import { Button, Dimensions, StyleSheet, View } from "react-native";
+import { useEvent } from "expo";
 
 const { width: SCREEN_WIDTH, height: SCREEN_HEIGHT } = Dimensions.get("window");
 
 export default function VideoPlayer() {
-  const videoRef = useRef<VideoRef>(null);
-  const videoSource = require("./77a071c3-c139-409c-bb6c-cfc00a09a9a2.mp4");
+	//const videoSource =
+	//	"https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4";
+	const assetId = require("./77a071c3-c139-409c-bb6c-cfc00a09a9a2.mp4");
 
-  return (
-    <Video
-      source={videoSource}
-      ref={videoRef}
-      style={styles.backgroundVideo} // Callback when remote video is buffering
-      onBuffer={() => {}}
-      // Callback when video cannot be loaded
-      onError={() => {}}
-    />
-  );
+	const videoSource: VideoSource = {
+		assetId,
+		metadata: {
+			title: "Test1",
+			artist: "Test2",
+		},
+	};
+
+	const player = useVideoPlayer(videoSource, (player) => {
+		player.loop = true;
+		player.play();
+	});
+
+	const { isPlaying } = useEvent(player, "playingChange", { isPlaying: player.playing });
+
+	return (
+		<View style={styles.contentContainer}>
+			<VideoView
+				style={styles.video}
+				player={player}
+				allowsFullscreen={false}
+				allowsPictureInPicture={false}
+			/>
+			<View style={styles.controlsContainer}>
+				<Button
+					title={isPlaying ? "Pause" : "Play"}
+					onPress={() => {
+						if (isPlaying) {
+							player.pause();
+						} else {
+							player.play();
+						}
+					}}
+				/>
+			</View>
+		</View>
+	);
 }
 
 // Later on in your styles..
+
 const styles = StyleSheet.create({
-  backgroundVideo: {
-    width: SCREEN_WIDTH,
-    position: "absolute",
-    top: 0,
-    left: 0,
-    bottom: 0,
-    right: 0,
-  },
+	contentContainer: {
+		flex: 1,
+		padding: 10,
+		alignItems: "center",
+		justifyContent: "center",
+		paddingHorizontal: 50,
+	},
+	video: {
+		width: 350,
+		height: 275,
+	},
+	controlsContainer: {
+		padding: 10,
+	},
 });
